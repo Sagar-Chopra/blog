411980f69f5684ae988b24b3907c68556e4e8478
Sagar-Chopra <sagarchopra271@gmail.com>
2025-03-08 12:15:24 +0530

    change-schema

--- src/app/api/(blogs)/bloglisting/route.js
+++ src/app/api/(blogs)/bloglisting/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true },
+  description: { type: String, required: true },
   creator: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
